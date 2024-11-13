e3de8f4eaa188b6a2c6f21fbe820c8fb50aa9345
Luke Yang <lukeyang8@gmail.com>
2024-11-14 03:39:50 +1100

    Added app frontend

--- client.py
+++ client.py
@@ -4,6 +4,29 @@ import threading
 import Crypto.Random
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP, AES
+from tkinter import scrolledtext
+from tkinter import *
+
+root = Tk()
+root.title("Chat")
+root.geometry("500x600")
+
+appHeader = Label(root, text="Encrypted Chat", bg = "blue", fg="white", font=("Georgia",24), height=1)
+appHeader.pack(fill=BOTH, expand=True)
+
+displayText = scrolledtext.ScrolledText(root, state=DISABLED, wrap=WORD)
+displayText.pack(fill=BOTH, expand=True)  
+
+inputText = scrolledtext.ScrolledText(root, state=NORMAL, wrap=WORD, height=5)
+inputText.pack(fill=BOTH, expand=True)
+
+
+#
+def displayMessage(message):
+    displayText.config(state=NORMAL)
+    displayText.insert(END, message + '\n')
+    displayText.config(state=DISABLED)
+    displayText.see(END)
 
 # Function to decode message
 def decodeMessage(socket, aesKey):
@@ -19,6 +42,7 @@ def decodeMessage(socket, aesKey):
             # ensure the integrity of the message
             decyptedMessage = cipherAes.decrypt_and_verify(ciphertext, tag)
             print('\n' + decyptedMessage.decode() + '\nYour message: ', end='')
+            displayMessage(decyptedMessage.decode())
         except:
             break
 
@@ -28,20 +52,102 @@ def encrypt_message_aes(aesKey, message):
     ciphertext, tag = cipher_aes.encrypt_and_digest(message)
     return cipher_aes.nonce + tag + ciphertext
 
+def send_message(username, socket, aesKey):
+
+    message = inputText.get("1.0", END).strip()
+    displayMessage("Me: " + message)
+    if message == 'exit':
+        socket.close()
+        sys.exit(0)
+    message = f"{username.capitalize()}: {message}"
+    message = encrypt_message_aes(aesKey, message.encode())
+    socket.send(message)
+    inputText.delete("1.0", END)
+
+
+def get_message():
+    message = inputText.get("1.0", END).strip()
+    if message == 'exit':
+        socket.close()
+        sys.exit(0)
+    return message
+
 # Generate RSA key pair
 key = RSA.generate(1024)
 privateKey = key.exportKey()
 publicKey = key.publickey().exportKey()
 
-
-# Connect to server 
+# Connect to server and get server key
 socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 socket.connect(('localhost', 8888))
+serverKey = socket.recv(1024)
+serverKey = RSA.importKey(serverKey)
+serverRsaCipher = PKCS1_OAEP.new(serverKey)
+displayMessage("Connected to Server ...")
+displayMessage("Enter your login details")
+authenticated = False
+
+# def login(socket, serverRsaCipher):
+#     def verify(username, password, socket, serverRsaCipher):
+#         message = f"auth {username} {password}"
+#         socket.send(serverRsaCipher.encrypt(message.encode()))
+#         res = socket.recv(1024)
+#         if res.decode() == "auth":
+#             print("Authenticated2")
+#             login.destroy()
+#         else:
+#             login.destroy()
+#             login()
+
+
+
+#     login = Tk()
+#     login.title("Login")
+#     login.geometry("200x200")
+
+#     Label(login, text="Username", bg = "lightblue", fg="white", font=("Georgia",15), height=1).pack(pady=3)
+#     username = Entry(login, show="*" ,width=10)
+#     username.pack(pady=5)
+
+#     Label(login, text="Password", bg = "lightblue", fg="white", font=("Georgia",15), height=1).pack(pady=3)
+#     password = Entry(login, show="*", width=10)
+#     password.pack(pady=5)
+
+#     Button(login, text="Login", bg="blue", fg="white", font=("Georgia",15), command=lambda: verify(username, password, socket, serverRsaCipher)).pack(pady=1)
+
+#     login.mainloop()
+
+
+# login()
+
+username = ""
+while not authenticated:
+    # displayMessage("Username: ")
+    # username = inputText.bind("<Return>", lambda x: get_message())
+    # displayMessage("Password: ")
+    # password = inputText.bind("<Return>", lambda x: get_message())
+    
+    username = input("Username: ")
+    username = username.replace(" ", "")
+    password = input("Password: ")
+    password = password.replace(" ", "")
+    message = f"auth {username} {password}"
+    socket.send(serverRsaCipher.encrypt(message.encode()))
+    res = socket.recv(1024)
+    if res.decode() == "auth":
+        authenticated = True
+    else:
+        print("Wrong login details, please try again")
+
+print("Authenticated")
+displayMessage("Authenticated")
+displayMessage("Waiting for other user ...")
+socket.recv(1024)
+
 
 # Exhange public keys
 socket.send(publicKey)
-print('Waiting for public key ...')
-otherPublicKey = socket.recv(4096)
+otherPublicKey = socket.recv(1024)
 otherPublicKey = RSA.importKey(otherPublicKey)
 otherRsaCipher = PKCS1_OAEP.new(otherPublicKey)
 
@@ -60,16 +166,21 @@ otherAesKey = myRsaCipher.decrypt(otherAesKeyEncrypted)
 thread = threading.Thread(target=decodeMessage, args=(socket, otherAesKey))
 thread.start()
 
+# print("Connected to server2")
+# thread2 = threading.Thread(target=send_message, args=(username, socket, aesKey))
+# thread2.start()
 
 # Send messages
-while True:
+# while True:
+#     send_message()
+displayMessage("... You may now start chatting ...")
+inputText.bind("<Return>", lambda x: send_message(username, socket, aesKey))
+root.mainloop()
 
-    message = input('Your message: ')
-    if message == 'exit':
-        break
-    message = "Recieved: " + message
-    message = encrypt_message_aes(aesKey, message.encode())
-    socket.send(message)
-    
+# root = Tk()
+# root.title("Chat")
+# root.geometry("500x700")  
+# displayText = scrolledtext.ScrolledText(root, state=DISABLED, wrap=WORD)
+# displayText.pack(fill=BOTH, expand=True)  
+# root.mainloop()
 
-socket.close()
--- server.py
+++ server.py
@@ -2,15 +2,38 @@ import socket
 import sys
 import threading
 import signal
+import Crypto.Random
+from Crypto.PublicKey import RSA
+from Crypto.Cipher import PKCS1_OAEP, AES
+
+clients = [
+    ["ben", "1234"],
+    ["jane", "5678"],
+    ["john", "helloworld"]
+]
+users = 0
+
+# generate RSA keys
+key = RSA.generate(1048)
+privateKey = key.exportKey()
+publicKey = key.publickey().exportKey()
+myRsaCipher = PKCS1_OAEP.new(RSA.importKey(privateKey))
 
 def handle_client(client, client2):
     while True:
         message = client.recv(1024)
+        
         if not message:
             break
         client2.send(message)
     
     client.close()
+
+# # if user leaves then listen for another one
+# def new_client():
+#     while users == 0: 
+
+
 
 def signal_handler(sig, frame):
     print('Closing server ...')
@@ -28,10 +51,35 @@ def main():
     signal.signal(signal.SIGINT, signal_handler)
 
     client1, addr1 = server.accept()
+    client1.send(publicKey)
+    while True:
+        message = client1.recv(1024)
+        message = myRsaCipher.decrypt(message)
+        username, password = message.decode().split()[1:]
+        print(username, password)
+        if [username, password] in clients:
+            client1.send("auth".encode())
+            break
+        else:
+            client1.send("not auth".encode())
     print('Client 1 connected: %s' % str(addr1))
     
     client2, addr2 = server.accept()
+    client2.send(publicKey)
+    while True:
+        message = client2.recv(1024)
+        message = myRsaCipher.decrypt(message)
+        username, password = message.decode().split()[1:]
+        print(username, password)
+        if [username, password] in clients:
+            client2.send("auth".encode())
+            break
+        else:
+            client2.send("not auth".encode())
     print('Client 2 connected: %s' % str(addr2))
+
+    client1.send("ready".encode())
+    client2.send("ready".encode())
     
     thread1 = threading.Thread(target=handle_client, args=(client1, client2))
     thread2 = threading.Thread(target=handle_client, args=(client2, client1))
@@ -41,7 +89,7 @@ def main():
     
     thread1.join()
     thread2.join()
-    
+
     socket.close()
 
 if __name__ == '__main__':
